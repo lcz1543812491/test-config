c96e5680f66eec335ecc4419851e2d867b804e0d
lcz <1543812491@qq.com>
2023-09-15 14:45:09 +0800

    change name

--- base/test/format.test.js
+++ base/test/format.test.js
@@ -1,19 +1,19 @@
 const { formatStorageNumberToObj } = require('../src/index')
 
-test('test number is 1024, decimal is 0,  expect result is number: 1, unit: KB', () => {
-  const { number, unit } = formatStorageNumberToObj(1024, 0)
-  expect(number).toBe(1)
-  expect(unit).toBe('KB')
+test('test number is 1024, decimal is 0, expect the result is number: 1, unit: KB', () => {
+    const { number, unit } = formatStorageNumberToObj(1024, 0)
+    expect(number).toBe(1)
+    expect(unit).toBe('KB')
 })
 
 test('test number is 1048576, decimal is 2,  expect result is number: 1, unit: MB', () => {
-  const { number, unit } = formatStorageNumberToObj(1048576, 0)
-  expect(number).toBe(1)
-  expect(unit).toBe('MB')
+    const { number, unit } = formatStorageNumberToObj(1048576, 0)
+    expect(number).toBe(1)
+    expect(unit).toBe('MB')
 })
 
 test('test number is 1248576, decimal is 2,  expect result is number: 1.19, unit: MB', () => {
-  const { number, unit } = formatStorageNumberToObj(1048576, 0)
-  expect(number).toBe(1)
-  expect(unit).toBe('MB')
+    const { number, unit } = formatStorageNumberToObj(1048576, 0)
+    expect(number).toBe(1)
+    expect(unit).toBe('MB')
 })
